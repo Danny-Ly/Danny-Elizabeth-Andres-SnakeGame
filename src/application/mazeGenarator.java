3a8685feb7f7035277d9ec2d469006eb79baa244
CS219-user <CS219-user@10.0.0.70>
2023-02-15 05:35:20 +0000

    maze generator class completly done, and javadocced with inline commenting

--- src/application/mazeGenarator.java
+++ src/application/mazeGenarator.java
@@ -1,43 +1,86 @@
 package application;
+import java.util.Random;
 
+/**
+ * This class is used in generating, customizing and creating a visual-like maze appearance 
+ * that user can visually see change according to user input.
+ * @author Written by Andres, assisted and co-written by Elizabeth and Danny.
+ *
+ */
 public class mazeGenarator extends Main {
+	// Initializing/declaring variables that will be used in the creation, 
+	// And customization of the maze
 	private static final int WALL = 2;
 	private static int EMPTY = 3;
 	private static int SNAKE = 1; 
 	private static int PELLET = 0;
 	int[][] maze;
 	int difficulty = 0;
-		
-		// Always be 10 by 20 just need to handle that
-		/*
-		 * Creating maze according to difficulty.
+	int numberOfobstactle = 0;
+	int numberOfPellets = 0;
+	Random randomvalue = new Random();
+	
+		/**
+		 * Creating a new 10 by 20 array that will behave as our maze.
 		 */
 		public void creation() {
-			// easy difficulty generated specific size array
-			if (difficulty == 0) {
+			// all difficulties generate specific size array
+			if (difficulty >= 0) {
 				maze = new int[10][20];
-			}
-			// medium difficulty generated specific size array
-			else if (difficulty == 1) {
-				//maze = new int[][];
-			}
-			// hard difficulty generated specific size array
-			else if (difficulty == 2) {
-				//maze = new int[][];
 			}
 			obstacles();
 		}
-		
+		/**
+		 * Altering the created 10 by 20 array into respective values to act like a maze.
+		 */
 		public void obstacles() {
-			// changes top row,and bottom row of desired array into walls
+			/*
+			 * Changing all initial values of the maze into empty spots.
+			 */
+			for(int i=0; i<maze.length; i++) {
+				for(int j=0; j <maze[i].length; j++) {
+					maze[i][j]= EMPTY;
+				}
+			}
+			// changes top row,and bottom row of maze into walls
 			for (int y_axis =0; y_axis < 20; y_axis++) {
 				maze[0][y_axis]= WALL;
 				maze[9][y_axis] = WALL;
 			}
-			// changes first column, and last column of desired array into walls
+			// changes first column, and last column of maze into walls
 			for (int x_axis = 0; x_axis < 10; x_axis++) {
 				maze[x_axis][0] = WALL;
 				maze[x_axis][19] = WALL;
+			}
+			/*
+			 * Randomly creating 20 or less obstacle within maze 
+			 */
+			while (numberOfobstactle <20) {
+				int x = randomvalue.nextInt(10) ;
+				int y = randomvalue.nextInt(16)+2 ;
+				if (x==2 || x==5 || x==7) {
+					maze[x][y]= WALL;
+					numberOfobstactle++;
+				}
+			}
+			// constant structure of every maze.
+			maze [4][9] = SNAKE;
+			maze [5][9] = WALL;
+			maze [5][8] = WALL;
+			maze [5][10] = WALL;
+			maze [4][8] = WALL;
+			maze [4][10] = WALL;
+			
+			/*
+			 * Randomly creating 5 or less pellets within maze
+			 */
+			while (numberOfPellets < 5) {
+				int x = randomvalue.nextInt(8)+1 ;
+				int y = randomvalue.nextInt(18)+1 ;
+				if(maze[x][y] != WALL && maze[x][y] != SNAKE) {
+					maze[x][y]= PELLET;
+					numberOfPellets++;
+				}
 			}
 			
 			/*
@@ -52,8 +95,11 @@ public class mazeGenarator extends Main {
 			boundary();
 		}
 		
+		/*
+		 * Changing the numeric values altered into a visual representation of each value 
+		 */
 		public void boundary(){
-			 
+			// looping through entire array changing each element into 1 of the four possible options.
 			for(int i=0; i<maze.length; i++) {
 				for(int j=0; j <maze[i].length; j++) {
 					if (maze [i][j] == WALL) {	
@@ -73,13 +119,9 @@ public class mazeGenarator extends Main {
 			}
 		}
 		
-		
-		
 	public static void main(String[] args) {
 			
 			mazeGenarator maze = new mazeGenarator();
 			maze.creation();	
-			
 	}
-	
 }
