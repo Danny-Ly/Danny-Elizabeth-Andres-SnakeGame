060a889559246278c8d986a799422e75420f0629
CS219-user <CS219-user@10.0.0.70>
2023-02-17 17:19:11 +0000

    snake class documenting done please check it over

--- src/application/Snake.java
+++ src/application/Snake.java
@@ -1,25 +1,34 @@
 package application;
-
+/**
+ * This class is handles when the user inputs WASD, the snake will move accordinly. 
+ * And will interact with the mazes randomly generated Walls and pellets,
+ * that will infuelnce the snakes action perfomed. 
+ * @author Written by Danny, assisted and co-written by Andres and Elizabeth.
+ *
+ */
 public class Snake {
+	// initializing all varibles that will be used in this class
 	public static int EMPTY = 0;
 	public static char WALL = 30000;
 	public static char PELLET = 10000;
 	public int difficulty = 0;
-	//public int counter = 0;
-	
 	public int SNAKE_LENGTH;
-
 	public int row_snake;
 	public int column_snake;
-	
 	public int column_movement;
 	public int row_movement;
-	
 	int [][] newSnakeMaze;
-	
 	String userInput;
 	
-	
+	/**
+	 * Constructor that is called in main that will infuence how the the snake is moving.
+	 * @param useInput a string value of the user input (WASD)
+	 * @param col_movement integer value representing the y-axis movment of snake
+	 * @param r_movement integer value representing the x-axis movement of snake 
+	 * @param col_snake integer value representing the y-axis postion of snake 
+	 * @param r_snake integer value representing the x-axis postiotion of snake
+	 * @param snakeLength length of snake
+	 */
 	public Snake(String useInput,int col_movement, int r_movement,int col_snake, int r_snake, int snakeLength) {
 		userInput = useInput;
 		
@@ -39,6 +48,12 @@ public class Snake {
 	// https://www.kosbie.net/cmu/fall-11/15-112/handouts/snake/
 	//snake.html#:~:text=In%20the%20game%20of%20Snake,food%2C%20and%20thereby%20grows%20larger
 	
+	/**
+	 * updates the postion/movment of snake in the maze based on user input
+	 * and the walls or pellets
+	 * @param pellets is from Rewards class that represents the pellets in the maze
+	 * @param mazeCreation is from MazeGenerator class that represents the walls in the maze
+	 */
 	public void movementofSnake(Rewards pellets, MazeGenerator mazeCreation) {
 
 		if (SNAKE_LENGTH >= 1) {
@@ -76,6 +91,10 @@ public class Snake {
 			}
 		}
 	}
+	/**
+	 * This checks if the snake has the same cordinates as a wall
+	 * @param mazeCreation is from MazeGenerator class that represent all objects in the maze
+	 */
 	public void runIntoWall (MazeGenerator mazeCreation ) {
 		if ((mazeCreation.maze[column_snake + column_movement][row_snake + row_movement])==(WALL)) {
 			System.out.println("GameOver");
@@ -84,6 +103,10 @@ public class Snake {
 			;
 		}
 	}
+	/**
+	 * This checks if the snake has the same corrdinates as a pellet
+	 * @param mazeCreation is from MazeGenerator class that represent all objects in the maze
+	 */
 	public void eatPellet (MazeGenerator mazeCreation ) {
 		if ((mazeCreation.maze[column_snake + column_movement][row_snake + row_movement])==(PELLET)) {
 			SNAKE_LENGTH+=1;
@@ -98,6 +121,10 @@ public class Snake {
 				
 		}
 	}
+	/**
+	 * This Checks if the snake has the same cordinates as itslef 
+	 * @param mazeCreation is from MazeGenerator class that represent all objects in the maze
+	 */
 	public void runIntoItself(MazeGenerator mazeCreation) {
 		if ((mazeCreation.maze[column_snake + column_movement][row_snake + row_movement])!=EMPTY) {
 			if((mazeCreation.maze[column_snake+column_movement][row_snake+ row_movement])<WALL){
@@ -112,6 +139,3 @@ public class Snake {
 		}
 	}
 }
-
-
-
