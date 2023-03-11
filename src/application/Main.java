8d221846b65e9686073d622c783077c32a571b8b
Danny <danny.ly1@ucalgary.ca>
2023-03-11 00:22:50 +0000

    updated for some strategies.

--- src/application/Main.java
+++ src/application/Main.java
@@ -54,6 +54,33 @@ public class Main extends Application{
 		enteredUserAction = userInputtedValue.getText();
 		System.out.println(enteredUserAction);
 		//userInteraction(enteredUserAction);
+		
+//		while (!(line.equalsIgnoreCase("easy"))) {
+//			System.out.println("Select difficulty:\n easy \n medium \n hard");
+//			line = inp.nextLine();
+//			line = line.toLowerCase().trim();
+//			if (!(line.equalsIgnoreCase("easy") || line.equalsIgnoreCase("medium")
+//					|| line.equalsIgnoreCase("hard"))) {
+//				System.out.println("Please enter a vaild user input. You typed: " + line); // adds user input to
+//																							// existing string and
+//																							// displays result
+//			}
+//
+//			if (line.equalsIgnoreCase("medium") || line.equalsIgnoreCase("hard")) {
+//				difficulty = 1;
+//				System.out.println("This Version is still in progress");
+//
+//			}
+//		}
+//
+//		if (line.equalsIgnoreCase("easy")) {
+//			difficulty = 0;
+//		}
+//		// generation of the MazeGenerator and printing of maze
+//		mazeCreation = new MazeGenerator(difficulty);
+//		snake = new Snake(mazeCreation);
+//		mazeCreation.boundary();
+		
 		try {
 			userInteraction(enteredUserAction);
 			//System.out.println("WINNER");
@@ -80,7 +107,6 @@ public class Main extends Application{
 		TextField userInputtedValue = new TextField();
 		Button userInputSnake = new Button ("Input");
 		
-	
 		userInputSnake.setOnAction(userInputAction -> getInputValue(userInputtedValue));
 		
 		allRows.getChildren().addAll(userInputtedValue,userInputSnake);
