ea8273fd5f5ad3103512fb53132b8311c02a2d82
Danny <danny.ly1@ucalgary.ca>
2023-03-12 09:20:15 +0000

    some extra stuff

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,9 +10,14 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.Node;
 
 /**
