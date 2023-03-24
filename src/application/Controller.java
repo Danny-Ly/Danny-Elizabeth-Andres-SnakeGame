69c3c50a5bcfbd15179a29cba9e013be9bb8810d
Danny <danny.ly1@ucalgary.ca>
2023-03-24 20:28:12 +0000

    cleaned up code to remove unnecessary code.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -126,43 +126,15 @@ public class Controller {
 
 		VBox allRows = new VBox();
 		HBox someHBox = new HBox();
-//		Button runGame = new Button ("Run Game!");  
-//		TextArea displayMaze = new TextArea("SNAKE GAME \n");
-//		
-//		// set the font so that everything is same size.
-//		// this snippet of code was referenced by:
-//		// https://docs.oracle.com/javafx/2/text/jfxpub-text.htm on specifically example 8.
-//		displayMaze.setFont(Font.font("Monospaced", FontWeight.BOLD, 16));
-//		
-//		// not allow the user to edit the display TextArea in the GUI. 
-//		// this code is referenced by:
-//		//https://stackoverflow.com/questions/20205145/javafx-how-to-show-read-only-text#:~:text=You%20can%20use%20following%20statement,setEditable(false)%3B
-//		//Answered by Ben on Dec 6, 2020, and edited by mahendrabishnoi2 on Mar 17, 2021. 
-//		displayMaze.setEditable(false);
-//		
-//		// change the dimensions of the TextArea in GUI
-//		//https://stackoverflow.com/questions/37458555/how-to-set-height-and-width-of-javafx-textarea
-//		// answered on Oct 12, 2017 by Michael Cenzoprano.
-//		displayMaze.setPrefHeight(240);
-//		displayMaze.setPrefWidth(250);
-//		
 		//https://docs.oracle.com/javafx/2/layout/size_align.htm#:~:text=Centering%20the%20Buttons,-The%20buttons%20are&text=hbButtons.,nodes%20within%20the%20HBox%20pane.
 		allRows.setAlignment(Pos. CENTER);
 		someHBox.setAlignment(Pos. CENTER);
 		
-		// https://stackoverflow.com/questions/33494052/javafx-redirect-console-output-to-textarea-that-is-created-in-scenebuilder
-		// alot of this code is from the reply of James_D on Nov 3, 2015 on how to redirect console output to a TextArea. 
-//		PrintStream printStream = new PrintStream(new DisplayOfGUIFromConsole(displayMaze), true);
-//        System.setOut(printStream);
-//        System.setErr(printStream);
-        
-        
         //https://stackoverflow.com/questions/8708342/redirect-console-output-to-string-in-java
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
     	PrintStream ps = new PrintStream(baos);
     	PrintStream old = System.out;
     	System.setOut(ps);
-    	
     	
 		mazeCreation = new MazeGenerator(difficultylocal);
 		snake = new Snake(mazeCreation);
@@ -185,8 +157,6 @@ public class Controller {
 		
 		transferStringToShape(baos,old);
 	
- 
-	
 		//based off of the code in this video:
 		// https://www.youtube.com/watch?v=tq_0im9qc6E&ab_channel=BroCode
 		gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -205,31 +175,6 @@ public class Controller {
 
 		
 	}
-		
-	/**
-	 * Allows for the printing output of the console and it shows this to the TextArea in the GUI.
-	 * (This code was from James_D on stack overflow [refer to the link below])
-	 * https://stackoverflow.com/questions/33494052/javafx-redirect-console-output-to-textarea-that-is-created-in-scenebuilder
-	 */
-	//https://stackoverflow.com/questions/33494052/javafx-redirect-console-output-to-textarea-that-is-created-in-scenebuilder
-	// this code is from the reply of James_D on Nov 3, 2015 on how to redirect console output to a TextArea. 
-//	public class DisplayOfGUIFromConsole extends OutputStream {
-//	        private TextArea textForGUI;
-//	        
-//	        DisplayOfGUIFromConsole(TextArea textForGUI) {
-//	            this.textForGUI = textForGUI;
-//	        }
-//	        
-//	        public void appendText(String valueOf) {
-//	            Platform.runLater(() ->textForGUI.appendText(valueOf));
-//	        }
-//	        
-//	        public void write(int b) throws IOException {
-//	            // Append the output to the TextArea
-//	       
-//	        	appendText(String.valueOf((char) b));
-//	        }
-//		} 
 	
 	/**
 	 * Sets the text in the difficulty display to "In development" 
@@ -266,13 +211,7 @@ public class Controller {
             someMaze[i] = mazeString[i].toCharArray();
         }
         
-        //CAN REMOVE THIS CODE WHEN DONE WITH THE TEXATAREA
-        for (int i = 0; i < someMaze.length; i++) {
-            for (int j = 0; j < someMaze[i].length; j++) {
-                System.out.print(someMaze[i][j]);
-            }
-            System.out.println();
-        }
+  
         // maybe change this to while loops?
         for (int rowMaze = 0; rowMaze < someMaze.length; rowMaze++) {
             for (int columnMaze = 0; columnMaze < someMaze[rowMaze].length; columnMaze++) {
@@ -280,7 +219,7 @@ public class Controller {
             	// First example used as a reference for height and width.
             	Rectangle someRect = new Rectangle();
             	someRect.setWidth(21);
-            	someRect.setHeight(22);
+            	someRect.setHeight(21);
             	
             	Circle someCirc = new Circle();
             	someCirc.setRadius(10);
